2e616a390b24d8aa0401edc4f0f56d342ee05107
RafaelBossh16 <rafaeltejeda769@gmail.com>
2024-12-23 18:51:43 -0500

    fix: cambio de tipo de parametro para la page de articles

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -4,15 +4,15 @@ export default function Articles() {
   const articles = [
     {
       id: 1,
-      title: "Article 1",
+      title: "Article 1"
     },
     {
       id: 2,
-      title: "Article 2",
+      title: "Article 2"
     },
     {
       id: 3,
-      title: "Article 3",
+      title: "Article 3"
     }
   ];
   return ( 
