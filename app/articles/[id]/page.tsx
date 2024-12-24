822e76f2298188e1888eaf7c8080bbec8f96344e
RafaelBossh16 <rafaeltejeda769@gmail.com>
2024-12-23 19:04:16 -0500

    fix: correccion de tipos en la pagina de articles

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,10 +1,15 @@
-export default function Articles({ params }: { params: { id: number } }) {
+export default async function Articles({
+  params
+}: {
+  params: Promise<{ id: number }>;
+}) {
+  const { id } = await params;
   return (
     <div>
       <h1>Page Page</h1>
       <p>
-        This is a page will be generated for each article with
-        the id: {params.id}
+        This is a page will be generated for each article with 
+        the id: {id}
       </p>
     </div>
   );
